515390001cee022915785ad93455aa0663c8dad6
John Norton <johnnorton@users.noreply.github.com>
2022-10-19 13:17:41 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
-echo $PLUGIN_ARTIFACT_FILE
+az blob push $PLUGIN_FILE_PATH
 
-cat >$PLUGIN_ARTIFACT_FILE << 'EOF'
+cat > $PLUGIN_ARTIFACT_FILE << 'EOF'
 {
   "kind": "fileUpload/v1",
   "data": {
     "fileArtifacts": [
       {
-        "name": "Docker",
-        "url": "https://www.google.com"
-      }
+        "name": "TESTRESULTS",
+        "url": "https://www.azureblob.com"
+      },
+      {
+        "name": "TESTRESULTS",
+        "url": "https://www.azureblob.com"
+      },
+      
     ]
   }
 }
 EOF
 
+echo "Pushed this to"
 cat $PLUGIN_ARTIFACT_FILE
